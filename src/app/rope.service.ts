32f8d131822ec769ef2c489ac49125fb975fd295
marvelwilson <mwtech56@gmail.com>
2023-05-30 04:30:13 +0100

    Desktop & Mobile app for gas sales management

--- src/app/rope.service.ts
+++ src/app/rope.service.ts
@@ -13,7 +13,7 @@ export class RopeService {
 
   constructor(public http: HttpClient) {
     this.ip = JSON.parse(localStorage.getItem('ip'));
-    this.url = "http://"+this.ip+"/gas/";
+    this.url = "http://"+this.ip+"/gas/"; //local ip
     this.ourl = "your baseUrl";
     console.log(this.url);
    }
